40b84d01325eb7fcaafda0065914349c49566dc7
ERNANI DE MIRANDA <ernani_dm@hotmail.com>
2020-08-10 22:30:45 -0300

    iniciando repositorio

---
+++ src/main/java/executavel/Executavel.java
@@ -0,0 +1,14 @@
+package executavel;
+
+import view.Menu;
+
+public class Executavel {
+	//Alteração feita via editor no GitHub
+	public static void main(String[] args) {
+		//Alterei localmente
+		Menu menu = new Menu();
+		menu.apresentarMenu();
+
+	}
+
+}
